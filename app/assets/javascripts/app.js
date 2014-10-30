cd840361dc3688cd4b24ab50a3795f73dfb2ed2c
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2014-10-30 16:44:09 -0700

    Angular added to rails

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,33 @@
+//=require_self
+//=require app.js
+//=require products.js
+
+
+(function(){
+  var app = angular.module('gemStore', ['store-directives']);
+
+  app.config(['$httpProvider', function ($httpProvider){
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+    $httpProvider.defaults.headers.common.Accept = 'application/json';
+  }]);
+
+  app.controller('StoreController', ['$http',function($http){
+    var store = this;
+    store.products = [];
+    $http.get('store-products.json').success(function(data){
+      store.products = data;
+    });
+  }]);
+
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+
+      this.review = {};
+    };
+  });
+
+
+})();
--- app/controllers/angular_controller.rb
+++ app/controllers/angular_controller.rb
@@ -1,2 +1,5 @@
 class AngularController < ApplicationController
+  def show
+
+  end
 end
